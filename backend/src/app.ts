994c4d201a1c25873d90b1963f969e3741355102
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-05 14:43:32 -0300

    feat: backend: create app class

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,22 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+
 class App {
-  constructor() {}
+  app: Application;
+
+  constructor() {
+    this.app = express();
+  }
+
+  initMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cors());
+  }
+
+  listen() {
+    this.app.listen(3333, () => console.log('Server is running on port 3333'));
+  }
 }
+
+export default App;
