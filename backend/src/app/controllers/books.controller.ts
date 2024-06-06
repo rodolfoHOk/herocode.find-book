58713d50dea36e59850f72fa847e99e79f785a49
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-05 22:25:19 -0300

    feat: backend: create methods find and update in books controller

--- backend/src/app/controllers/books.controller.ts
+++ backend/src/app/controllers/books.controller.ts
@@ -13,10 +13,41 @@ class BooksController {
       if (!book) {
         return { status: 400, message: 'Missing body' };
       }
-
       const response = await this.booksUseCases.createBook(book);
-
       return { status: 201, message: 'Book created', data: response };
+    } catch (error: any) {
+      return { status: 400, message: error.message };
+    }
+  }
+
+  async find(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      const queryParams: BookDto = request.query;
+      const response = await this.booksUseCases.findBook(queryParams);
+      if (!response) {
+        return { status: 404, message: 'Book not found' };
+      }
+      return { status: 200, message: 'Book found', data: response };
+    } catch (error: any) {
+      return { status: 400, message: error.message };
+    }
+  }
+
+  async update(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      const id: string = request.params.id;
+      if (!id) {
+        return { status: 400, message: 'Missing param id' };
+      }
+      const book: BookDto = request.body;
+      if (!book) {
+        return { status: 400, message: 'Missing body' };
+      }
+      const response = await this.booksUseCases.updateBook(id, book);
+      if (!response) {
+        return { status: 404, message: 'Book not found' };
+      }
+      return { status: 200, message: 'Book updated', data: response };
     } catch (error: any) {
       return { status: 400, message: error.message };
     }
